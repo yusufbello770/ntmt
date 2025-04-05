6e5e6a5c1d1fa66a85cd7e1f0de7f904a5fd1282
yusufbello770 <yusufbello770@gmail.com>
2025-04-05 09:22:48 -0400

    Initial commit

--- core/detectors/bandwidth.py
+++ core/detectors/bandwidth.py
@@ -1,3 +1,22 @@
-#!/usr/bin/python3
+from collections import defaultdict
+from core.utils.constants import BANDWIDTH_THRESHOLD
 
-print("give me a bottle of rum!")
+class BandwidthDetector:
+    def __init__(self):
+        self.usage = defaultdict(lambda: {'upload': 0, 'download': 0})
+
+    def track_usage(self, packet):
+        if IP in packet:
+            src = packet[IP].src
+            dst = packet[IP].dst
+            length = len(packet)
+            self.usage[src]['upload'] += length
+            self.usage[dst]['download'] += length
+
+    def check_thresholds(self):
+        alerts = []
+        for ip, stats in self.usage.items():
+            total = stats['upload'] + stats['download']
+            if total > BANDWIDTH_THRESHOLD:
+                alerts.append(f"Bandwidth misuse: {ip} used {total//1024//1024} MB")
+        return alerts
--- core/utils/constants.py
+++ core/utils/constants.py
@@ -1,3 +1,5 @@
-#!/usr/bin/python3
-
-print("give me a bottle of rum!")
+# Detection thresholds
+BANDWIDTH_THRESHOLD = 10 * 1024 * 1024  # 10 MB/min
+PORT_SCAN_THRESHOLD = 15                 # 15 SYN packets
+REQUEST_RATE_THRESHOLD = 100             # 100 requests/sec
+SQL_INJECTION_REGEX = r"('|--|; DROP|1=1|UNION SELECT)"
